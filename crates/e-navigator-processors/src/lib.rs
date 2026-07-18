ea2b46be0d811bce77cf596787ff2dab2b33f311
Victor Bona <victor.bona@hotmail.com>
2026-07-18 13:40:56 -0300

    fix(resources): enforce workload capture policy

--- crates/e-navigator-processors/src/lib.rs
+++ crates/e-navigator-processors/src/lib.rs
@@ -2,7 +2,9 @@
 #![warn(missing_debug_implementations, rust_2018_idioms, unreachable_pub)]
 
 pub mod container_attribution;
+mod workload_resource_filter;
 
 pub use container_attribution::{
     ContainerAttributionProcessor, KubernetesMetadataCache, KubernetesMetadataProvider,
 };
+pub use workload_resource_filter::WorkloadResourceFilterProcessor;
